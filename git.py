ec1e2baaba9721a5699b8f0f15475202c9edadc8
Svati Priya <swatipriya98484@gmail.com>
2024-05-26 15:29:16 +0530

    first commit

---
+++ git.py
@@ -0,0 +1,5 @@
+width = float(input("Enter the width of rectangle:")) 
+height = float(input("Enter the height of rectangle:"))
+Area = width * height
+
+print("The area of the rectangle is:", Area)
